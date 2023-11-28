f227a09a20915890ddd8e058b4de639686ff805b
Gabriele Trevisan <gabriele.3vi@gmail.com>
2023-11-28 17:30:32 +0100

    First commit
    Created sveltekit project

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,7 @@
+<script>
+    import "../app.css";
+</script>
+
+<div class="w-9/12 m-auto p-2.5">
+    <slot />
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+
+</script>
+
+<h1 class="text-3xl underline text-center">
+    Digital Business Modelling test
+</h1>
+
+<div class="avatar">
+    <div class="w-24 rounded">
+      <img src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+    </div>
+  </div>
