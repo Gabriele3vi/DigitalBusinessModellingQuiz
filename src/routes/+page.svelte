38cf5a8853962d85531afd51f666ad81aff1c407
Gabriele Trevisan <gabriele.3vi@gmail.com>
2023-11-29 16:29:28 +0100

    Corrected some questions

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,6 +8,7 @@
 
   let questions = []
   let numRandomQuest = 15
+  let buttonMsg = "Take the test"
 
   //questo metodo ritorna una lista di interi random diversi di lunghezza data nel range [min, max]
   function getRandomIntSet(size, min, max){
@@ -34,6 +35,7 @@
       questions.push(data.questions[n])
     }
     started = true
+    buttonMsg = "Change questions"
   }
 </script>
 
@@ -48,12 +50,20 @@
       <label for="qnumber" class="">Choose the number of questions:</label>
       <input name="qnumber" type="number" min="1" max="30" step="1" bind:value={numRandomQuest} class="input input-bordered input-sm w-min max-w-xs" />
     </div>
-    <button class="btn" on:click={start_test}>Take the test</button>
+    <p class="text-sm"><span class="font-bold">NB:</span> anwers can be wrong</p>
+    <button class="btn" on:click={start_test}> 
+      {#if started}
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99" />
+      </svg> 
+      {/if}
+      {buttonMsg} 
+    </button>
   </div>
   <h1 class="text-3xl font-bold">Questions</h1>
   <div class="flex flex-col gap-y-5">
     {#if !started}
-    <h3>Questions will be displayed here</h3>
+    <h3>Click the button to generate some questions</h3>
     {:else}
      {#each questions as q (q.qnumber)}
       <Question {...q}/>
