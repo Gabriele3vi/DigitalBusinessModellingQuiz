ad2f1b55d0a3e1567f891892f1d79644214305bc
Gabriele Trevisan <gabriele.3vi@gmail.com>
2023-12-06 16:40:31 +0100

    fix

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -60,11 +60,6 @@
 
     let testEl = document.getElementById('test')
     
-    testEl.scrollIntoView({
-      behavior: 'smooth',
-      block: 'start'
-
-    });
   }
 
   function searchQuestions() {
@@ -107,11 +102,20 @@
   <h1 class="text-5xl font-bold text-center my-10">
       DBM test simulator
   </h1>
-  <div class="flex flex-col gap-y-2">
+  <div class="flex flex-col gap-y-2.5">
     <div class="flex flex-row gap-x-2.5 items-center">
       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-7 h-7">
         <path stroke-linecap="round" stroke-linejoin="round" d="M12 6.042A8.967 8.967 0 006 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 016 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 016-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0018 18a8.967 8.967 0 00-6 2.292m0-14.25v14.25" />
       </svg>        
+    <h2 class="text-3xl font-bold">Download the answers</h2>
+    </div>
+    <p>To download the file, click <a href="questions.json" class="underline font-bold" download>here</a>.</p>
+  </div>
+  <div class="flex flex-col gap-y-2 mt-3.5">
+    <div class="flex flex-row gap-x-2.5 items-center">
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-7 h-7">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
+      </svg>
     <h2 class="text-3xl font-bold">Look for specific questions</h2>
     </div>
     <p>Type the numbers separated by a comma to search for specific questions.</p>
