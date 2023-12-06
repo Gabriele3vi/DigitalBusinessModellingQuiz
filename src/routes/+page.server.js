10429f571c69173196da3404ee17637898b7b705
Gabriele Trevisan <gabriele.3vi@gmail.com>
2023-12-06 16:37:50 +0100

    Added the possibility to download the file

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,5 +1,5 @@
 export async function load({ fetch }) {
-    const response = await fetch('q.json');
+    const response = await fetch('questions.json');
     const q = await response.json();
 
     //console.log(typeof(questions))
