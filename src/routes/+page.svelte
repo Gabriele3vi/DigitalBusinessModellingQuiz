9bd903cd02c5aa0fb099894416b317a0c40243cd
Gabriele Trevisan <gabriele.3vi@gmail.com>
2023-12-15 11:40:15 +0100

    Question fix

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -164,7 +164,7 @@
     
     <div class="flex flex-col md:flex-row gap-x-1.5 md:items-center">
     <p>Is the simulator helpful for you? </p>
-    <a class="font-medium italic flex flex-row items-center gap-x-1.5 hover:underline text-blue-700" href="https://www.paypal.com/paypalme/Gabriele3vi"> Offer me a coffee!
+    <a class="font-medium italic flex flex-row items-center gap-x-1.5 underline text-blue-700" href="https://www.paypal.com/paypalme/Gabriele3vi"> Offer me a coffee!
       <svg xmlns="http://www.w3.org/2000/svg" class="w-3 h-3" viewBox="0 0 512 512">
         <path stroke="#1D4ED8" d="M432 320H400a16 16 0 0 0 -16 16V448H64V128H208a16 16 0 0 0 16-16V80a16 16 0 0 0 -16-16H48A48 48 0 0 0 0 112V464a48 48 0 0 0 48 48H400a48 48 0 0 0 48-48V336A16 16 0 0 0 432 320zM488 0h-128c-21.4 0-32.1 25.9-17 41l35.7 35.7L135 320.4a24 24 0 0 0 0 34L157.7 377a24 24 0 0 0 34 0L435.3 133.3 471 169c15 15 41 4.5 41-17V24A24 24 0 0 0 488 0z"/>
       </svg>
